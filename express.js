8b097c1a0f612cfd4239ddc56e3f696c08aa828a
unknown <kamatabhishek14@gmail.com>
2023-10-29 19:53:00 +0530

    added how to send response in the form of JSON

--- express.js
+++ express.js
@@ -7,14 +7,25 @@ const app = express() //create a express server object
 
 // now the only thing is here we will not be sending the listener function instead we will listening to the requests like
 
+
 app.get("/home",(req,res)=>{
     // here we have send instead of end
-    res.send("welcome to express GET method")
+   // res.send("welcome to express GET method")
+
+    // suppose if we want to send JSON then
+      res.json({
+        msg:"this is GET method"
+      })
 })
 
 app.post("/home",(req,res)=>{
     // here we have send instead of end
-    res.send("welcome to express POST method")
+    // res.send("welcome to express POST method")
+
+    // suppose if we want to send JSON
+    res.json({
+        msg:"this is POST method"
+    })
 })
 
 // through postman if we make these requests we can see these outputs
