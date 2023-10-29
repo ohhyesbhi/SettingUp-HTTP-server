df2b127cd10b6b092be69616a98b9ff1ca37e473
unknown <kamatabhishek14@gmail.com>
2023-10-29 19:49:24 +0530

    added how to set up the server using express js

---
+++ express.js
@@ -0,0 +1,24 @@
+const express =require("express")
+// now this express is nothing but a function
+
+const PORT = 4000;
+
+const app = express() //create a express server object
+
+// now the only thing is here we will not be sending the listener function instead we will listening to the requests like
+
+app.get("/home",(req,res)=>{
+    // here we have send instead of end
+    res.send("welcome to express GET method")
+})
+
+app.post("/home",(req,res)=>{
+    // here we have send instead of end
+    res.send("welcome to express POST method")
+})
+
+// through postman if we make these requests we can see these outputs
+
+app.listen(PORT,()=>{
+  console.log("it is running on PORT number",PORT)
+})
